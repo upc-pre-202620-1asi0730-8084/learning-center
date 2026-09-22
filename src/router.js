aaa7ae6046f2187ef923a49f3c5637899fbbc71e
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-22 18:34:38 -0500

    feat(home): add Home component with translation support and navigation route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import {createRouter, createWebHistory} from "vue-router";
+import Home from "./shared/presentation/views/home.vue";
 
-const routes = [];
+const routes = [
+    { path: "/home", name: "home", component: Home, meta: {title: "Home"}}
+];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),

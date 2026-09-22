1b5524477a4fc2ded80b1b49d792b3a88266f769
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-22 18:45:34 -0500

    feat(about): add About component with translation support and navigation route

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Home from "./shared/presentation/views/home.vue";
+const about = () => import("./shared/presentation/views/about.vue");
 
 const routes = [
-    { path: "/home", name: "home", component: Home, meta: {title: "Home"}}
+    { path: "/home", name: "home", component: Home, meta: {title: "Home"}},
+    { path: "/about", name: "about", component: about, meta: {title: "About Us"}}
 ];
 
 const router = createRouter({

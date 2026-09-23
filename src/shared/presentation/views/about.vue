246132acd45a69eecd37e1ea36f16500e035ff79
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-22 19:28:23 -0500

    feat(about): add ACME logo to about component

--- src/shared/presentation/views/about.vue
+++ src/shared/presentation/views/about.vue
@@ -8,6 +8,7 @@ const {t} = useI18n();
 <section class="pt-6 p-4 md:p-5">
   <div class="flex flex-column gap-3">
     <h1 class="text-4xl font-bold text-color">{{ t('about.title') }}</h1>
+    <img src="/acme-logo.svg" alt="ACME Logo" class="w-12rem h-auto border-round-md"/>
     <p class="m-0 line-height-3 text-color-secondary">{{ t('about.content') }}</p>
   </div>
 </section>
